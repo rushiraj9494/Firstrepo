c37b115aed7a0ee5f0ae8ad43d3e48c51a86de5c
rushikesh <omkardhawale2000@gmail.in>
2023-03-28 17:28:14 +0530

    merge karaycha aahe hare krishana

---
+++ hareKrishana.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+cout <<"hare krishana hare krishana krishana krishana hare hare hare ram hare ram ram ram hare hare 
+";
+
+    
+   return 0;
+}
